bb7db2235f2b6b00ae01a2f8dda8f5ec77eb40d1
Timo <napokue@hotmail.com>
2017-12-09 15:03:34 -0800

    loading sector 2 and 3 from disk

--- 16bit/boot_sect.asm
+++ 16bit/boot_sect.asm
@@ -1,30 +1,32 @@
 [org 0x7c00]
-	mov bx, HELLO_MSG
-	call print
+    mov bp, 0x8000 ; set the stack safely away from us
+    mov sp, bp
 
-	call print_nl
+    mov bx, 0x9000 ; es:bx = 0x0000:0x9000 = 0x09000
+    mov dh, 2 ; read 2 sectors
+    ; the bios sets 'dl' for our boot disk number
+    ; if you have trouble, use the '-fda' flag: 'qemu -fda file.bin'
+    call disk_load
 
-	mov bx, GOODBYE_MSG
-	call print
+    mov dx, [0x9000] ; retrieve the first loaded word, 0xdada
+    call print_hex
 
-	call print_nl
+    call print_nl
 
-	mov dx, 0x12fe
-	call print_hex
+    mov dx, [0x9000 + 512] ; first word from second loaded sector, 0xface
+    call print_hex
 
-jmp $								; Infinite jump
+    jmp $
 
 %include "boot_sect_print.asm"
 %include "boot_sect_print_hex.asm"
+%include "boot_sect_disk.asm"
 
-HELLO_MSG: db 'Hello, World!', 0
-GOODBYE_MSG: db 'Goodbye!', 0
+; Magic number
+times 510 - ($-$$) db 0
+dw 0xaa55
 
-times 510-($-$$) db 0				; When compiled, our program must fit into 512 bytes,
-									; with the last two bytes being the magic number,
-									; so here, tell our assembly compiler to pad out our
-									; program with enough zero bytes (db 0) to bring us to the
-									; 510th byte.
-
-dw 0xaa55							; Last two bytes (one word) form the magic number,
-									; so BIOS knows wer are a boot sector.
+; boot sector = sector 1 of cyl 0 of head 0 of hdd 0
+; from now on = sector 2 ...
+times 256 dw 0xdada ; sector 2 = 512 bytes
+times 256 dw 0xface ; sector 3 = 512 bytes
