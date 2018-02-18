96f73adcd29e374925bbb4fcc09968dd076169c5
Timo <napokue@hotmail.com>
2018-02-18 12:10:18 -0800

    update panic function

--- src/lib.rs
+++ src/lib.rs
@@ -14,12 +14,17 @@ mod vga_buffer;
 
 #[no_mangle]
 pub extern fn rust_main(multiboot_information_address: usize) {
-    // ATTENTION: we have a very small stack and no guard page
-    vga_buffer::clear_screen();
-    println!("Hello \nWorld");
-
+    
     loop{}
 }
 
 #[lang = "eh_personality"] extern fn eh_personality() {}
-#[lang = "panic_fmt"] #[no_mangle] pub extern fn panic_fmt() -> ! {loop{}}
+
+#[lang = "panic_fmt"] 
+#[no_mangle] 
+pub extern fn panic_fmt(fmt: core::fmt::Arguments, file: &'static str,
+    line: u32) -> ! {
+    println!("\n\nPANIC in {} at line {}:", file, line);
+    println!("    {}", fmt);
+    loop{}
+}
