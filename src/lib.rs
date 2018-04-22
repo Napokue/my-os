1a4a7a8dfbe872817f4bc2d04b384295ccbd372f
Timo de Kort <napokue@hotmail.com>
2018-04-22 16:21:45 +0200

    start cross compilation of the cross

--- src/lib.rs
+++ src/lib.rs
@@ -1,68 +1,29 @@
 #![feature(lang_items)]
-#![feature(const_fn)]
-#![feature(ptr_internals)]
-#![feature(unique)]
 #![no_std]
+#![no_main]
 
-extern crate rlibc;
-extern crate volatile;
-extern crate spin;
-extern crate multiboot2;
-
-#[macro_use]
-mod vga_buffer;
-
-mod memory;
-
+// Linux
 #[no_mangle]
-pub extern fn rust_main(multiboot_information_address: usize) {
-    use memory::FrameAllocator;
-
-    vga_buffer::clear_screen();
-    println!("Welcome to Mios{}", "!");
-    let boot_info = unsafe{multiboot2::load(multiboot_information_address)};
-    let memory_map_tag = boot_info.memory_map_tag()
-        .expect("Memory map tag required");
-
-    let elf_sections_tag = boot_info.elf_sections_tag()
-        .expect("Elf-sections tag required");
-
-    let kernel_start = elf_sections_tag.sections().map(|s|s.addr)
-        .min().unwrap();
-
-    let kernel_end = elf_sections_tag.sections().map(|s|s.addr + s.size)
-        .max().unwrap();
-
-    let multiboot_start = multiboot_information_address;
-    let multiboot_end = multiboot_start + (boot_info.total_size as usize);
-
-    println!("start{:x}", kernel_start);
-    println!("end{:x}", kernel_end);
-    
-    println!("start{:x}", multiboot_start);
-    println!("end{:x}", multiboot_end);
-
-    let mut frame_allocator = memory::AreaFrameAllocator::new(
-        kernel_start as usize, kernel_end as usize, 
-        multiboot_start as usize, multiboot_end as usize, 
-        memory_map_tag.memory_areas());
-    for i in 0.. {
-        if let None = frame_allocator.allocate_frame() {
-            println!("allocated {} frames", i);
-            break;
-        }
-    }
-
-    loop{}
+pub extern "C" fn _start() -> ! {
+    loop {}
 }
 
-#[lang = "eh_personality"] extern fn eh_personality() {}
+// Windows
+#[no_mangle]
+pub extern "C" fn mainCRTStartup() -> ! {
+    main()
+}
 
-#[lang = "panic_fmt"] 
-#[no_mangle] 
-pub extern fn panic_fmt(fmt: core::fmt::Arguments, file: &'static str,
-    line: u32) -> ! {
-    println!("\n\nPANIC in {} at line {}:", file, line);
-    println!("    {}", fmt);
-    loop{}
+// MacOS
+#[no_mangle]
+pub extern "C" fn main() -> ! {
+    loop {}
+}
+
+#[lang = "panic_fmt"]
+#[no_mangle]
+pub extern "C" fn rust_begin_panic(_msg: core::fmt::Arguments,
+    _file: &'static str, _line: u32, _column: u32) -> !
+{
+    loop {}
 }
